e327359206b6039520e341965e6ce0ed784047d8
MatthewMurawski <matthew.murawski@slu.edu>
2024-04-28 13:50:49 -0500

    Clearing old stuff from Beta branch

--- GameMechanicsTest/Background.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class Background {
-    
-    public Background(gamePanel panel){
-        ImageIcon icon = new ImageIcon("3veowxtcmq471.jpg");   // uses one style of loading images
-        panel.add(new JLabel(icon));
-    }
-}
